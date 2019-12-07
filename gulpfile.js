36de6253a1da15d59e78903a19a0a14ac5979183
horimislime <horimislime@gmail.com>
2019-12-07 12:33:59 +0900

    Migrate all images to Google Photos

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,6 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     concat = require('gulp-concat');
 
-gulp.task('compress', function () {
-    return gulp.src('./_images/**')
-        .pipe(imagemin([
-          imagemin.jpegtran({progressive: true}),
-          imagemin.optipng({optimizationLevel: 5}),
-          imagemin.svgo({plugins: [{removeViewBox: true}]})
-        ]))
-        .pipe(gulp.dest('./images'));
-});
-
 gulp.task('minify', function() {
   return gulp.src('stylesheets/*.css')
     .pipe(cleanCSS({inline: ['local']}))
