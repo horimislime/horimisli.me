83ffb80b53a51ed47de5d2f3d86b91653c059c55
horimislime <horimislime@gmail.com>
2021-01-19 01:06:26 +0900

    Make tags descriptive

--- lib/gen.dart
+++ lib/gen.dart
@@ -7,62 +7,7 @@ import 'package:universal_html/driver.dart';
 import 'package:universal_html/html.dart';
 import 'package:universal_html/prefer_universal/html.dart';
 import 'models/site.dart';
-// import 'dart:html';
-
-extension Interpolation on String {
-  String injected(Map<String, dynamic> data) {
-    return this.splitMapJoin(RegExp(r'{.*?}'),
-        onMatch: (match) => data[match.group(0)].toString() ?? '');
-  }
-}
-
-extension Html on Element {
-  static Element header({String id, String className, List<Element> children}) {
-    return Element.tag('header')
-      ..id = id
-      ..className = className
-      ..nodes = children;
-  }
-
-  static DivElement div(
-      {String id, String className, List<Element> children, String innerText}) {
-    // return Element.div()
-    //   ..id = id
-    //   ..className = className
-    //   ..nodes = children
-    //   ..innerText = innerText;
-    return element('div', id: id, className: className, children: children);
-  }
-
-  static Element a({String href, String className, String innerText}) {
-    return Element.a()
-      ..setAttribute('href', href)
-      ..innerText = innerText;
-  }
-
-  static Element nav(String className, {String id, List<Element> children}) {
-    return element('nav', id: id, className: className, children: children);
-  }
-
-  static Element element(String name,
-      {String id,
-      String className,
-      List<Element> children,
-      String innerText,
-      String innerHtml}) {
-    final tag = Element.tag(name);
-    if (id != null) tag.id = id;
-    if (className != null) tag.className = className;
-    if (innerText != null) tag.innerText = innerText;
-    if (innerHtml != null) tag.innerHtml = innerHtml;
-    if (children != null) {
-      for (final child in children) {
-        tag.append(child);
-      }
-    }
-    return tag;
-  }
-}
+import 'html_syntax.dart';
 
 class Page {
   final String title;
@@ -84,46 +29,39 @@ class CustomUriPolicy implements UriPolicy {
   bool allowsUri(String uri) => true;
 }
 
-class DefaultLayout implements Layout {
-  final NodeValidatorBuilder _htmlValidator = NodeValidatorBuilder.common()
-    ..allowElement('a',
-        attributes: ['href', 'data-size', 'data-show-count'],
-        uriPolicy: CustomUriPolicy())
-    ..allowElement('iframe',
-        attributes: ['scrolling', 'src'], uriPolicy: CustomUriPolicy())
-    ..allowElement('img', attributes: ['src'], uriPolicy: CustomUriPolicy())
-    ..allowElement('script',
-        attributes: [
-          'async',
-          'charset',
-          'src',
-          'type',
-          'data-id',
-          'data-ratio'
-        ],
-        uriPolicy: CustomUriPolicy())
-    ..allowElement('div', attributes: ['role', 'data-dest'])
-    ..allowElement('blockquote', attributes: [
-      'data-instgrm-version',
-      'data-instgrm-permalink',
-      'data-instgrm-captioned'
-    ])
-    ..allowElement('g', attributes: [
-      'stroke',
-      'transform',
-      'fill',
-      'fill-rule',
-      'stroke-width',
-      'viewbox',
-      'version',
-      'height',
-      'width'
-    ])
-    ..allowElement('svg')
-    ..allowElement('time', attributes: ['datetime'])
-    ..allowSvg()
-    ..allowInlineStyles();
+final NodeValidatorBuilder htmlValidator = NodeValidatorBuilder.common()
+  ..allowElement('a',
+      attributes: ['href', 'data-size', 'data-show-count'],
+      uriPolicy: CustomUriPolicy())
+  ..allowElement('iframe',
+      attributes: ['scrolling', 'src'], uriPolicy: CustomUriPolicy())
+  ..allowElement('img', attributes: ['src'], uriPolicy: CustomUriPolicy())
+  ..allowElement('script',
+      attributes: ['async', 'charset', 'src', 'type', 'data-id', 'data-ratio'],
+      uriPolicy: CustomUriPolicy())
+  ..allowElement('div', attributes: ['role', 'data-dest'])
+  ..allowElement('blockquote', attributes: [
+    'data-instgrm-version',
+    'data-instgrm-permalink',
+    'data-instgrm-captioned'
+  ])
+  ..allowElement('g', attributes: [
+    'stroke',
+    'transform',
+    'fill',
+    'fill-rule',
+    'stroke-width',
+    'viewbox',
+    'version',
+    'height',
+    'width'
+  ])
+  ..allowElement('svg')
+  ..allowElement('time', attributes: ['datetime'])
+  ..allowSvg()
+  ..allowInlineStyles();
 
+class DefaultLayout implements Layout {
   String get meta => '''
   
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -149,63 +87,43 @@ class DefaultLayout implements Layout {
         : '';
   }
 
-  String body(String injectionContent) => '''
-  <div class="site-wrap">
-    <header class="site-header px2 px-responsive">
-      <div class="mt2 wrap">
-        <div class="measure">
-          <a href="/" class="site-title">${site.title}</a>
-          <nav class="site-nav">
-            <a href="/about">About</a>
-          </nav>
-          <div class="clearfix"></div>
-        </div>
-      </div>
-    </header>
-    <div class="post p2 p-responsive wrap" role="main">
-      <div class="measure">
-        ${injectionContent}
-      </div>
-    </div>
-  </div>
-  <footer class="center">
-    <div class="measure">
-      <small>
-        Powered by <a href="https://github.com/horimislime/horimisli.me">horimislime/horimisli.me</a>
-      </small>
-    </div>
-  </footer>
-  ''';
+  List<Node> _buildHead() {
+    return [
+      element('meta'),
+    ];
+  }
 
-  HtmlDocument build({String injectionContent = ''}) {
-    return HtmlDriver().document
-      ..head.appendHtml(meta, validator: _htmlValidator)
-      ..body.appendHtml(body(injectionContent), validator: _htmlValidator)
-      ..body.nodes = [
-        Html.div(className: 'site-wrap', children: [
-          Html.header(className: 'site-header px2 px-responsive', children: [
-            Html.div(className: 'mt2 wrap', children: [
-              Html.div(className: 'measure', children: [
-                Html.a(
-                    href: '/', className: 'site-title', innerText: site.title),
-                Html.nav('site-nav',
-                    children: [Html.a(href: '/about', innerText: 'About')]),
-                Html.div(className: 'clearfix')
-              ])
+  Node _buildBody(Node innerContent) {
+    return div(children: [
+      div(className: 'site-wrap', children: [
+        header(className: 'site-header px2 px-responsive', children: [
+          div(className: 'mt2 wrap', children: [
+            div(className: 'measure', children: [
+              a(href: '/', className: 'site-title', innerText: site.title),
+              nav('site-nav',
+                  children: [a(href: '/about', innerText: 'About')]),
+              div(className: 'clearfix')
             ])
-          ]),
-          Html.div(className: 'post p2 p-responsive wrap', children: [
-            Html.div(className: 'measure', innerText: injectionContent)
           ])
         ]),
-        Html.element('footer', className: 'center', children: [
-          Html.div(className: 'measure', children: [
-            Html.element('small',
-                innerHtml:
-                    'Powered by <a href="https://github.com/horimislime/horimisli.me">horimislime/horimisli.me</a>')
-          ])
+        div(className: 'post p2 p-responsive wrap', children: [
+          element('div', className: 'measure', children: [innerContent])
         ])
-      ]
+      ]),
+      element('footer', className: 'center', children: [
+        div(className: 'measure', children: [
+          element('small',
+              innerHtml:
+                  'Powered by <a href="https://github.com/horimislime/horimisli.me">horimislime/horimisli.me</a>')
+        ])
+      ])
+    ]);
+  }
+
+  HtmlDocument build({Node innerContent}) {
+    return HtmlDriver().document
+      ..head.appendHtml(meta, validator: htmlValidator)
+      ..body.append(_buildBody(innerContent))
       ..body
           .append(ScriptElement()..src = '/resources/vendor/highlight.min.js');
   }
@@ -226,35 +144,53 @@ class EntryLayout extends DefaultLayout {
   @override
   EntryLayout({this.page, this.site});
 
-  String get content => '''
-  <div class="post-header mb2">
-  <h1>${page.title}</h1>
-  <span class="post-meta">{{ page.date | date: site.date_format }}</span><br>
-  {% if page.update_date %}
-    <span class="post-meta">{{ page.update_date | date: site.date_format }}</span><br>
-  {% endif %}
-  </div>
-  <article class="post-content">
-    ${page.content}
-  </article>
-  <div class="share-links">
-    <div id="twitter-button">
-        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" data-size="large">Tweet</a>
-    </div>
-    <div id="tippin-button" data-dest="horimislime"></div>
-  </div>
-  <div class="py2 post-footer">
-    <a href="https://twitter.com/horimislime" target="_blank"><img src="/images/slime.jpg" alt="horimislime" class="avatar" /></a>
-  </div>
-  <script src="/resources/vendor/highlight.min.js"></script>
-  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-  <script src="https://tippin.me/buttons/tip.js?0001" type="text/javascript"></script>
-  <script>hljs.initHighlightingOnLoad();</script>
-  ''';
+  Node _buildContent() {
+    return div(children: [
+      div(className: 'ost-header mb2', children: [
+        element('h1', innerText: page.title),
+        element('span',
+            className: 'post-meta',
+            innerText: page.publishedAt.toIso8601String()),
+      ]),
+      element('article', className: 'post-content', innerHtml: page.content),
+      div(className: 'share-links', children: [
+        div(id: 'twitter-button', children: [
+          a(
+              href: 'https://twitter.com/share?ref_src=twsrc%5Etfw',
+              className: 'twitter-share-button',
+              attributes: {'data-show-count': 'false', 'data-size': 'large'},
+              innerText: 'Tweet')
+        ]),
+        element('div',
+            id: 'tippin-button', attributes: {'data-dest': 'horimislime'}),
+      ]),
+      div(className: 'py2 post-footer', children: [
+        element('a', attributes: {
+          'href': 'https://twitter.com/horimislime',
+          'target': '_blank'
+        }, children: [
+          element('img',
+              className: 'avatar', attributes: {'src': '/images/slime.jpg'})
+        ])
+      ]),
+      element('script',
+          attributes: {'src': '/resources/vendor/highlight.min.js'}),
+      element('script', attributes: {
+        'src': 'https://platform.twitter.com/widgets.js',
+        'async': '',
+        'charset': 'utf-8'
+      }),
+      element('script', attributes: {
+        'src': 'https://tippin.me/buttons/tip.js?0001',
+        'type': 'text/javascript'
+      }),
+      element('script', innerText: 'hljs.initHighlightingOnLoad();'),
+    ]);
+  }
 
   @override
-  HtmlDocument build({String injectionContent = ''}) {
-    return super.build(injectionContent: content);
+  HtmlDocument build({Node innerContent}) {
+    return super.build(innerContent: _buildContent());
   }
 }
 
