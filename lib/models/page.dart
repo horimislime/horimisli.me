ae79e0c1c2e9ea5ee437bd0727fd8f7ac1461dd9
horimislime <horimislime@gmail.com>
2021-01-25 00:30:49 +0900

    Refactor codes

---
+++ lib/models/page.dart
@@ -0,0 +1,8 @@
+class Page {
+  final String title;
+  final DateTime publishedAt;
+  final List<String> categories;
+  final String content;
+  final String url;
+  Page(this.title, this.publishedAt, this.categories, this.content, this.url);
+}
---
+++ lib/templates/layout.dart
@@ -0,0 +1,5 @@
+abstract class Layout {
+  final Site site;
+  final Page page;
+  Layout({@required this.site, @required this.page});
+}
