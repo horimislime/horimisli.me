17053ef1374a205243a35f1035c4187ea886f1d6
horimislime <horimislime@gmail.com>
2021-03-06 23:42:12 +0900

    Create entry list

--- lib/gen.dart
+++ lib/gen.dart
@@ -4,22 +4,34 @@ import 'package:blog/templates/default.dart';
 import 'package:front_matter/front_matter.dart' as frontmatter;
 import 'package:markdown/markdown.dart' as markdown;
 import 'models/site.dart';
-import 'models/page.dart';
-import 'templates/post.dart';
+import 'package:path/path.dart' as path;
 
 class Post {
   String title;
   String htmlBody;
   DateTime publishedDate;
   List<String> categories;
+  String pathName;
 
-  Post._(this.title, this.htmlBody, this.publishedDate, this.categories);
+  Post._(this.title, this.htmlBody, this.publishedDate, this.categories,
+      this.pathName);
 
   static Post loadFromFile(String filePath) {
     final raw = io.File(filePath).readAsStringSync(encoding: utf8);
     final document = frontmatter.parse(raw);
     final htmlBody = markdown.markdownToHtml(document.content);
-    return Post._(document.data['title'], htmlBody, DateTime.now(), ['test']);
+    final fileName = path.posix.basenameWithoutExtension(filePath);
+    return Post._(
+        document.data['title'], htmlBody, DateTime.now(), ['test'], fileName);
+  }
+
+  static List<Post> list(String directoryPath) {
+    final files = io.Directory(directoryPath).listSync();
+    final markdowns = files.where((f) {
+      final ext = path.posix.extension(f.path);
+      return ext == '.md' || ext == '.markdown';
+    });
+    return markdowns.map((f) => loadFromFile(f.path)).toList();
   }
 }
 
@@ -27,28 +39,26 @@ void main() {
   final site = Site('horimisli.me', 'horimislime', 'horimislime\'s blog',
       'horimisli.me', 'horimisli.me');
 
-  final postDir = io.Directory('_posts');
-  final files = postDir.listSync();
-  for (final file in files) {
-    final regex =
-        RegExp('[0-9]{4}\-[0-9]{2}\-[0-9]{2}\-([0-9a-zA-Z\-]+)\.(md|markdown)');
-    final match = regex.firstMatch(file.path);
-    if (match == null) {
-      print('Skipping file ${file.path}');
-      continue;
-    }
-
-    final entryName = match.group(1);
-    final post = Post.loadFromFile(file.path);
+  final posts = Post.list('_posts');
+  for (final post in posts) {
     final test = PostPage();
     final renderedHtml = test.render(site, post);
 
-    final outputDirectory = io.Directory('_site/entry/$entryName');
+    final outputDirectory = io.Directory('_site/entry/${post.pathName}');
     if (!outputDirectory.existsSync()) {
       outputDirectory.createSync();
     }
 
-    final output = io.File('_site/entry/$entryName/index.html');
+    final output = io.File('_site/entry/${post.pathName}/index.html');
     output.writeAsStringSync(renderedHtml);
   }
+
+  final indexData = IndexPageData()
+    ..posts = posts
+    ..site = site
+    ..title = site.title;
+  final indexPage = IndexPage();
+  final renderedHtml = indexPage.render(indexData);
+  final output = io.File('_site/index.html');
+  output.writeAsStringSync(renderedHtml);
 }
