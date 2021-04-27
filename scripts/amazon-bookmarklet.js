704750c6d52b79f3e8a14d3f4e8a815ceb82ae87
horimislime <horimislime@gmail.com>
2021-04-27 12:10:58 +0900

    Save bookmarklet for Amazon

---
+++ scripts/amazon-bookmarklet.js
@@ -0,0 +1,23 @@
+javascript: (() => {
+  const productUrl = document
+    .querySelector('link[rel="canonical"]')
+    .href.replace(/amazon.co.jp\/.*\/dp/, 'amazon.co.jp/dp');
+  const imageUrl = document.querySelector('#main-image-container img')
+    .attributes['src'].textContent;
+  const title = document.querySelector('#productTitle').innerText;
+  const detailElement = document.querySelector('#bylineInfo');
+  const detail = detailElement == undefined ? '' : detailElement.innerText;
+  const elements =
+    '<div class="flex items-center m-4"><div class="amazon-image"><a href="' +
+    productUrl +
+    '" target="_blank"> <img src="' +
+    imageUrl +
+    '" class="h-40"> </a> </div> <div class="m-8"> <p>' +
+    title +
+    '<br/>' +
+    detail +
+    '</p> <p class="underline font-medium"> <a href="' +
+    productUrl +
+    '" target="_blank">Amazon の商品ページへ</a> </p> </div> </div>';
+  alert(elements);
+})();
