653c0a7e36fe1a53d76aeb5988dd7c03f12c2081
horimislime <horimislime@gmail.com>
2021-08-05 21:19:07 +0900

    Add custom 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(): JSX.Element {
+  return (
+    <main className="prose m-4">
+      <h1>404 - Page Not Found</h1>
+      <a href="/">Back to Home</a>
+    </main>
+  );
+}
