495d8d22847e12640bc31b48c00fd0bbedc551b7
horimislime <horimislime@gmail.com>
2022-01-10 23:59:40 +0900

    Remove unnecessary overrides

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   content: ['./src/**/*.tsx', './posts/blog/**/*.md'],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
