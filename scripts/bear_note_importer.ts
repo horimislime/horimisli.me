e3cc1aa471c0dd2207c49e6f858b9b7763b59adf
horimislime <horimislime@gmail.com>
2022-02-18 23:50:00 +0900

    wip

--- scripts/bear_note_importer.ts
+++ scripts/bear_note_importer.ts
@@ -1,4 +1,6 @@
 import * as path from 'path';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
 export {};
 
@@ -11,19 +13,25 @@ function readBearNote(filePath: string): BearNote {
   return { body: '', images: [] };
 }
 
-const main = () => {
+(async () => {
   console.log('hello');
-  const filePath = process.argv[2];
-  const fileExtension = path.extname(filePath);
-  console.log(process.argv);
-  console.log(`ext: ${fileExtension}`);
-  if (fileExtension === '.txt') {
-    console.log('read text');
-  } else if (fileExtension === '.bearnote') {
-    console.log('note with images');
-  } else {
-    console.log('invalid file format');
-  }
-};
+  // const filePath = process.argv[2];
+  // const fileExtension = path.extname(filePath);
+  // console.log(process.argv);
+  // console.log(`ext: ${fileExtension}`);
+  // if (fileExtension === '.txt') {
+  //   console.log('read text');
+  // } else if (fileExtension === '.bearnote') {
+  //   console.log('note with images');
+  // } else {
+  //   console.log('invalid file format');
+  // }
 
-main();
+  const rl = readline.createInterface({ input, output });
+
+  const slug = await rl.question('slug:');
+
+  console.log(`File name will be posts/${slug}.md`);
+
+  rl.close();
+})();
