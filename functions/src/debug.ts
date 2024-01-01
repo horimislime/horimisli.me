484e008c4d9d2d34fb45b9a8ef90b14de514182c
horimislime <horimislime@gmail.com>
2024-01-01 13:33:11 +0900

    Fix error

--- functions/src/debug.ts
+++ functions/src/debug.ts
@@ -3,6 +3,5 @@ import sharp from 'sharp';
 
 (async () => {
   const modifiedImageBuffer = await resize('../public/images/2023/sc_tasks_org.png', 'large');
-  modifiedImageBuffer.
   await sharp(modifiedImageBuffer, { animated: true }).toFile('./test_out.webp');
 })();
