f2a4ca1d2aaf8a5db50006a445bbd42601d63a52
horimislime <horimislime@gmail.com>
2024-04-21 22:50:19 +0900

    Customize style of typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            li: {
+              '& p': {
+                marginTop: '0.5em',
+                marginBottom: '0.5em',
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
   content: ['./src/**/*.tsx', './posts/blog/**/*.md'],
   plugins: [
     require('@tailwindcss/typography'),
