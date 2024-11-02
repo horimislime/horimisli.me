28980e62c609d30be13da890d78864fc4f3d1511
horimislime <horimislime@gmail.com>
2024-11-02 23:01:50 +0900

    Solve import problem

--- src/entities/Entry.ts
+++ src/entities/Entry.ts
@@ -5,11 +5,41 @@ import matter from 'gray-matter';
 import path from 'path';
 import rehypeRaw from 'rehype-raw';
 import stringify from 'rehype-stringify';
+import type { Parser } from 'unified';
 import { unified } from 'unified';
+import type { OrgNode } from 'uniorg';
 import { extractKeywords } from 'uniorg-extract-keywords';
+import { ParseOptions } from 'uniorg-parse/lib/parse-options';
+import { parse as uparse } from 'uniorg-parse/lib/parser.js';
 import uniorg2rehype from 'uniorg-rehype';
 
-import { orgParse } from '../UniorgUtil';
+const options: ParseOptions = {
+  todoKeywords: ['TODO', 'DONE'],
+  useSubSuperscripts: '{}',
+  // Interestingly enough, zero-width space (\u200b) is not considered
+  // a space in unicode but is considered a space by Emacs. This is
+  // why we have to add \u200b explicitly after \s in the
+  // regex. Otherwise, the suggested use-case of adding ZWSP as a
+  // markup border does not work.
+  emphasisRegexpComponents: {
+    // deviates from org mode default to allow ndash, mdash, and
+    // quotes (’“”)
+    pre: '-–—\\s\u200b\\(\'’"“”\\{',
+    // deviates from org mode default to allow ndash, mdash, and
+    // quotes (’“”)
+    post: '-–—\\s\u200b.,:!?;\'’"“”\\)\\}\\[',
+    border: '\\s\u200b',
+    body: '.',
+    newline: 1,
+  },
+  linkTypes: ['https', 'http'],
+  matchSexpDepth: 3,
+};
+
+export function orgParse(this: any) {
+  const parser: Parser<OrgNode> = (_doc, file) => uparse(file, options);
+  Object.assign(this, { Parser: parser });
+}
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 const hiddenCategories = ['share', 'blog'];
