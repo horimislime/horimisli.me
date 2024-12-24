b1e7f13a40e278284aee0a6b3f86bb66cb797239
horimislime <horimislime@gmail.com>
2024-12-24 09:42:15 +0900

    Resize buttons

--- src/pages/entry/[id].tsx
+++ src/pages/entry/[id].tsx
@@ -112,19 +112,19 @@ const EntryPage: NextPage<Props> = (props) => {
             href={`https://bsky.app/intent/compose?text=${encodeURIComponent(`${props.entry.title} / https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}/entry/${props.entry.id}`)}`}
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-bsBlue text-white px-4 py-2 rounded flex items-center"
+            className="bg-bsBlue text-white px-2 py-1 rounded flex items-center no-underline"
           >
-            <img src="https://storage.googleapis.com/horimislime-static/images/generated/bluesky_media_kit_logo.svg" alt="Bluesky Logo" className="w-6 h-6 mr-2" />
+            <img src="https://storage.googleapis.com/horimislime-static/images/generated/bluesky_media_kit_logo.svg" alt="Bluesky Logo" className="w-4 h-4 mr-2" />
             Bluesky
           </a>
           <a
             href={`https://b.hatena.ne.jp/add?url=${encodeURIComponent(`https://${process.env.NEXT_PUBLIC_SITE_DOMAIN}/entry/${props.entry.id}`)}`}
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-hbBlue text-white px-4 py-2 rounded flex items-center"
+            className="bg-hbBlue text-white px-2 py-1 rounded flex items-center no-underline"
           >
             <img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" />
-            はてなブックマーク
+            Hatena
           </a>
 
         </div>
