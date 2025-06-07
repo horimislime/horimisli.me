da8da22e0f2c3f8fd3b62360ce389cc5fb074e4e
horimislime <horimislime@gmail.com>
2025-06-07 18:28:44 +0900

    remove ts-node related settings

--- functions/src/debug.ts
+++
@@ -1,7 +0,0 @@
-import { resize } from './index';
-import sharp from 'sharp';
-
-(async () => {
-  const modifiedImageBuffer = await resize('../public/images/2023/sc_tasks_org.png', 'large');
-  await sharp(modifiedImageBuffer, { animated: true }).toFile('./test_out.webp');
-})();
